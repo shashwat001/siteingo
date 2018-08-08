1cd21553d4c5918c018aed711302c685e370dae0
Shashwat Kumar <shashwatkmr.001@gmail.com>
2018-08-09 00:27:47 +0530

    etag test

--- index.go
+++ index.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
+	"strings"
 )
 
-
 func serveHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content Type", "text/html")
-	e := "testetag"
-	http.ServeFile(w, r, "./static/")
-	w.Header().Set("Etag", e)
+	w.Header().Set("Content Type", "text/html")
 	w.Header().Set("Cache-Control", "max-age=2592000") // 30 days
-	log.Println('This is reaching sendHTTP')
+	e := "testetag"
+	w.Header().Set("Etag", e)
+	http.ServeFile(w, r, "./static/")
+	log.Println("This is reaching sendHTTP")
 
-	//if match := r.Header.Get("If-None-Match"); match != "" {
-		//if strings.Contains(match, e) {
-			//w.WriteHeader(http.StatusNotModified)
-			//return
-		//}
-	//}
+	if match := r.Header.Get("If-None-Match"); match != "" {
+		if strings.Contains(match, e) {
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
+	}
 }
 
 func main() {
