cbea162c64f34a8cd766c1dc13fc85d8ee82b832
Shashwat Kumar <shashwatkmr.001@gmail.com>
2018-08-09 00:35:07 +0530

    multiple headers write issue resolved

--- index.go
+++ index.go
@@ -7,19 +7,20 @@ import (
 )
 
 func serveHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content Type", "text/html")
-	w.Header().Set("Cache-Control", "max-age=2592000") // 30 days
 	e := "testetag"
-	w.Header().Set("Etag", e)
-	http.ServeFile(w, r, "./static/")
-	log.Println("This is reaching sendHTTP")
-
 	if match := r.Header.Get("If-None-Match"); match != "" {
 		if strings.Contains(match, e) {
 			w.WriteHeader(http.StatusNotModified)
 			return
 		}
 	}
+
+	w.Header().Set("Etag", e)
+	w.Header().Set("Content Type", "text/html")
+	w.Header().Set("Cache-Control", "max-age=2592000") // 30 days
+	http.ServeFile(w, r, "./static/")
+	log.Println("This is reaching sendHTTP")
+
 }
 
 func main() {
